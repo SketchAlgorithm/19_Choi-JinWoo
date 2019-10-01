c074647961cecf91e4f4afdea8e115dd52b017b7
SketchAlgorithm <47266632+SketchAlgorithm@users.noreply.github.com>
2019-10-01 19:22:13 +0900

    Update root.py

--- root.py
+++ root.py
@@ -3,8 +3,10 @@ import math
 for t in range(int(input())):
     n = int(input())
     cnt = 0
-        if n == 2:
+
     while True:
+        if n == 2:
+            cnt = 0
             break
         # n이 1일때 처리를 안해주면 무한루프를 돈다.
         if n == 1:
@@ -19,4 +21,4 @@ for t in range(int(input())):
             cnt += (pow(tmp + 1, 2) - n)  # n보다 큰 최소의 제곱수에서 n을 빼서 횟수에 추가
             cnt += 1  # 제곱근 구하는 횟수 추가
             n = int(math.sqrt(n)) + 1
-        c
+    print(cnt)
